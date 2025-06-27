dcec80f74b59f27490f3957b74220c3e0aeec6eb
Katya <k.vezzadini@gmail.com>
2025-06-27 09:41:16 +0100

    Adding a silly little toast

--- src/counter/Counter.tsx
+++ src/counter/Counter.tsx
@@ -1,41 +1,59 @@
 import React from 'react';
 import { Button, HStack, Text, VStack } from '@chakra-ui/react';
 import { useCounter } from './useCounter';
+import { Toaster, toaster } from "@/components/ui/toaster"
 
 export const Counter: React.FC = () => {
   const { state, dispatch } = useCounter();
 
+  const handleIncrement = () => {
+    dispatch({ type: 'INCREMENT' });
+
+    // Show toast if new value will be 10
+    if (state.present + 1 === 10) {
+        toaster.create({
+        description: 'Congrats! You made it to 10!',
+        duration: 3000
+      });
+    }
+  };
+
   return (
-    <VStack  align="center" mt={10}>
-      <Text fontSize="4xl" fontWeight="bold">
-        {state.present}
-      </Text>
+    <>
+        <VStack  align="center" mt={10}>
+        <Text fontSize="4xl" fontWeight="bold">
+            {state.present}
+        </Text>
 
-      <HStack >
-        <Button colorPalette="cyan" variant="surface" onClick={() => dispatch({ type: 'INCREMENT' })}>
-          Increment
-        </Button>
-        <Button colorPalette="cyan" variant="surface" onClick={() => dispatch({ type: 'DECREMENT' })}>
-          Decrement
-        </Button>
-      </HStack>
+        <HStack >
+            <Button colorPalette="cyan" variant="surface" onClick={handleIncrement}>
+            Increment
+            </Button>
+            <Button colorPalette="cyan" variant="surface" onClick={() => dispatch({ type: 'DECREMENT' })}>
+            Decrement
+            </Button>
+        </HStack>
 
-      <HStack >
-        <Button
-          colorPalette="cyan" variant="outline"
-          onClick={() => dispatch({ type: 'UNDO' })}
-          disabled={state.past.length === 0}
-        >
-          Undo
-        </Button>
-        <Button
-          colorPalette="cyan" variant="outline"
-          onClick={() => dispatch({ type: 'REDO' })}
-          disabled={state.future.length === 0}
-        >
-          Redo
-        </Button>
-      </HStack>
-    </VStack>
+        <HStack >
+            <Button
+            colorPalette="cyan" variant="outline"
+            onClick={() => dispatch({ type: 'UNDO' })}
+            disabled={state.past.length === 0}
+            >
+            Undo
+            </Button>
+            <Button
+            colorPalette="cyan" variant="outline"
+            onClick={() => dispatch({ type: 'REDO' })}
+            disabled={state.future.length === 0}
+            >
+            Redo
+            </Button>
+        </HStack>
+        </VStack>
+        <Toaster />
+    </>
+
+    
   );
 };
